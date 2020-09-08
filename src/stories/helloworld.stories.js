acf2e69ce7044803bf6426e94c3e6fbe68267cf1
sschoe85 <s@bine-schoetensack.eu>
2020-09-08 12:51:05 +0200

    Test changes to story

--- src/stories/helloworld.stories.js
+++ src/stories/helloworld.stories.js
@@ -6,6 +6,6 @@ export default {
 
 export const exampleStory = () => (
   <div style={{ padding: "16px", backgroundColor: "#eeeeee" }}>
-    <h1 style={{ color: "rebeccapurple" }}>Hello from Storybook and Gatsby!</h1>
+    <h1 style={{ color: "black" }}>Hello from Storybook and Gatsby!</h1>
   </div>
 )
