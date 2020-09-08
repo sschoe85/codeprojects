3df082bf5beff2a77d862d7c786bbe290ceeef89
sschoe85 <s@bine-schoetensack.eu>
2020-09-08 12:45:58 +0200

    retry test story

--- src/stories/helloworld.stories.js
+++ src/stories/helloworld.stories.js
@@ -1,1 +1,11 @@
-<p>Hello World</p>
+import React from "react"
+
+export default {
+  title: "Dashboard/header",
+}
+
+export const exampleStory = () => (
+  <div style={{ padding: "16px", backgroundColor: "#eeeeee" }}>
+    <h1 style={{ color: "rebeccapurple" }}>Hello from Storybook and Gatsby!</h1>
+  </div>
+)
