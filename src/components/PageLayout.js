47560de88fb436458f992f6f30cfa2148bc6c71e
sschoe85 <s@bine-schoetensack.eu>
2020-09-09 14:19:50 +0200

    Moved Header and Footer to components folder and set PageLayout

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,12 +1,18 @@
 //General Page Layout with Header, Children and Footer
 
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 function PageLayout(props) {
     return (
         <div>
-            
+            <Header/>
+            {props.children}
+            <Footer/>
         </div>
+            
+        
     );
 }
 
