3099759ec23cf49fb74f41919cc96086841f22fc
sschoe85 <s@bine-schoetensack.eu>
2020-09-10 12:36:54 +0200

    Installed Gatsby File System Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
 
 module.exports = {
   siteMetadata:{
@@ -10,5 +5,13 @@ module.exports = {
     author: 'Sabine Schoetensack'
   },
  
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    }
+  ],
 }
