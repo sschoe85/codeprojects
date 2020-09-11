df662115d8656140b531b70474724a5ef529d83f
sschoe85 <s@bine-schoetensack.eu>
2020-09-11 10:23:31 +0200

    First tests of faqCard component working with react spring

---
+++ src/pages/faq.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import FaqCard from "../components/FaqCard"
+
+function Faq(props) {
+  
+
+  return <FaqCard/>
+}
+
+export default Faq
