f5d773d5b989d5e691ef9ea83a8e74d37e8b66da
sschoe85 <s@bine-schoetensack.eu>
2020-09-13 21:14:46 +0200

    First Tests with Faq Section

--- src/components/FaqCard.js
+++ src/components/FaqCard.js
@@ -3,27 +3,39 @@ import { useSpring, animated } from "react-spring"
 import styled from "@emotion/styled"
 
 function FaqCard(props) {
-  const Card = styled(animated.div)`
-    background-color: ${props => (props.front ? "hotpink" : "turquoise")};
-    font-size: 22px;
-    font-weight: bold;
+  const Card = styled.div`
+    display: grid;
+    grid-column: span 4;
+  `
+  const CardFront = styled(animated.div)`
+    display: flex;
+    flex-direction: column;
+    background-image: linear-gradient(
+      90deg,
+      rgba(63, 94, 251, 1) 0%,
+      rgba(252, 70, 107, 1) 100%
+    );
+    cursor: pointer;
     color: white;
   `
-  const CardFrontContent = styled.div`
+  const CardBack = styled(animated.div)`
+    display: flex;
+    flex-direction: column;
+    background-image:linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
   `
 
   const [flipped, setFlipped] = useState(false)
   const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px), rotateX(${flipped ? 180 : 0})deg`,
+    config: { mass: 5, tension: 500, friction: 80 },
   })
   return (
-    <div onClick={() => setFlipped(!flipped)}>
+    <Card onClick={() => setFlipped(!flipped)}>
       <CardFront
-        front
         style={{ opacity: opacity.interpolate(o => 1 - o), transform }}
       >
-        <CardFrontContent></CardFrontContent>
+        <h2>{props.question}</h2>
       </CardFront>
       <CardBack
         style={{
@@ -31,10 +43,10 @@ function FaqCard(props) {
           transform: transform.interpolate(t => `${t} rotateX(180deg)`),
         }}
       >
-
-        <CardBackContent></CardBackContent>
+        
+        <p>{props.answer}</p>
       </CardBack>
-    </div>
+    </Card>
   )
 }
 
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,11 +1,27 @@
 import React from "react"
-
+import GlobalStyles from "../components/GlobalStyles"
 import FaqCard from "../components/FaqCard"
+import styled from "@emotion/styled"
 
 function Faq(props) {
-  
+const FaqSection = styled.section`
+display: grid;
+grid-template-columns: repeat(12, minmax(auto, 60px));
+grid-gap: 40px;`
 
-  return <FaqCard question = "Hallo" answer = "Tschüss!"/>
+
+  return (
+    <GlobalStyles>
+      <FaqSection>
+        <FaqCard question = "Wie geht's?" answer = "Super!"/>
+        <FaqCard question = "Das ist ein Test blablablablabaaaaaa aaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaaa" answer = "sdgd sgsg sg sdgsgsg sdgsdg  sdgsdg"/>
+        <FaqCard />
+        <FaqCard question = "Test" answer = "Test"/>
+        <FaqCard />
+        <FaqCard />
+      </FaqSection>
+    </GlobalStyles>
+  )
 }
 
 export default Faq
