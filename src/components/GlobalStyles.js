830c5c2e2692037ad04f2acd606711b0ece721a9
sschoe85 <s@bine-schoetensack.eu>
2020-09-14 14:52:01 +0200

    Fixed faq Cards & Card Section

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -13,7 +13,7 @@ export default function GlobalStyles({ children }) {
           *:after {
             box-sizing: border-box;
             padding: 0;
-            margin: 5px;
+            margin: 0;
           }
           html {
             background: #111;
