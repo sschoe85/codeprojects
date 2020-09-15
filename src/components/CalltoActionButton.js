9f22da63ffd76572e066a8cf786c04c92699bf95
sschoe85 <s@bine-schoetensack.eu>
2020-09-15 10:07:31 +0200

    Call to Action Button setup

--- src/components/CalltoActionButton.js
+++ src/components/CalltoActionButton.js
@@ -1,1 +1,17 @@
+import styled from "@emotion/styled"
 //Gets passed a prop LinkTo that leads to the desired section, exists in two different styles based on its props (https://emotion.sh/docs/styled)
+
+import React from "react"
+
+const ButtonSmall = styled(ButtonLarge)``
+const ButtonLarge = styled.button``
+
+function CalltoActionButton(size, linkTo) {
+  if (props.size === "small") {
+    return <ButtonSmall onClick={handleClick}></ButtonSmall>
+  } else {
+    return <ButtonLarge onClick={handleClick}></ButtonLarge>
+  }
+}
+
+export default CalltoActionButton
