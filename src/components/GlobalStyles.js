0d5bcc08f2c454097307459ec023f5ea5a9ef1d5
sschoe85 <s@bine-schoetensack.eu>
2020-09-15 12:20:12 +0200

    oved variables to root Element

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,36 +3,38 @@ import { Global, css } from "@emotion/core"
 
 export default function GlobalStyles() {
   return (
-    
-      <Global
-        styles={css`
-          @import url("https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap");
-          @import url("https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap");
-          *,
-          *:before,
-          *:after {
-            box-sizing: border-box;
-            padding: 0;
-            margin: 5px;
-          }
-          html {
-            background: #111;
-            color: #fff;
-            font-family: "Solway", serif;
-            --gradient: linear-gradient(101deg, #78e4ff, #ff48fa);
-            --gradientSecondary: linear-gradient(0deg, rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 100%);
-            
-            
-          }
-          h1,
-          h2,
-          h3 {
-            font-family: "Finger Paint", cursive;
-            
-          }
+    <Global
+      styles={css`
+        @import url("https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap");
+        @import url("https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap");
 
-          
-        `}
-      />
+        :root {
+          --gradient: linear-gradient(101deg, #78e4ff, #ff48fa);
+          --gradientSecondary: linear-gradient(
+            0deg,
+            rgba(34, 193, 195, 1) 0%,
+            rgba(253, 187, 45, 1) 100%
+          );
+        }
+        *,
+        *:before,
+        *:after {
+          box-sizing: border-box;
+          padding: 0;
+          margin: 5px;
+        }
+        html {
+          background: #111;
+          color: #fff;
+          font-family: "Solway", serif;
+        }
+
+        h1,
+        h2,
+        h3 {
+          font-family: "Finger Paint", cursive;
+        }
+      `}
+    />
   )
 }
