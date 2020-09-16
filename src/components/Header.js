5b0f5aa226f2a14c3edf07b32b19146e5738423e
sschoe85 <s@bine-schoetensack.eu>
2020-09-16 11:12:53 +0200

    Styled Header, renamed button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,87 @@
-import styled from '@emotion/styled';
-import React,{useEffect} from 'react';
+import styled from "@emotion/styled"
+import { Link } from "gatsby"
+import React, { useEffect } from "react"
+import MenuIcon from "../assets/hamburgerMenuIcon.png"
 
 const StyledHeader = styled.header`
-  width: 100%;
-  min-height: 10vh;
+  width: 100vw;
+  min-height: 9vh;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: linear-gradient(0deg, rgba(48,59,110,1) 0%, rgba(17,16,0,1) 100%);
-  transition: all .7s ease-in;
+  background: var(--gradient);
+  transition: all 0.7s ease-in;
   margin-bottom: 1rem;
 `
 const StyledScrolledHeader = styled(StyledHeader)`
-position: fixed;
+  position: fixed;
   top: 0;
   left: 0;
-  background-color: lightblue;
   z-index: 999;
-  
+`
+const HeaderElement = styled.nav`
+  padding: 0.7rem 0.2rem;
+  display: flex;
+  margin: 0 1rem;
+  justify-content: space-between;
+  & > a {
+    text-decoration: none;
+    color: white;
+    font-family: "Finger Paint";
+    font-size: calc(1rem + 1vw);
+  }
+  & > a > img {
+    height: 8vh;
+  }
 `
 
-
-const Header=(props) => {
-  const [scrolled,setScrolled]=React.useState(false);
-const handleScroll=() => {
-    const offset=window.scrollY;
-    if(offset > 200 ){
-      setScrolled(true);
-    }
-    else{
-      setScrolled(false);
+const Header = props => {
+  const [scrolled, setScrolled] = React.useState(false)
+  const handleScroll = () => {
+    const offset = window.scrollY
+    if (offset > 200) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
     }
   }
 
   useEffect(() => {
-    window.addEventListener('scroll',handleScroll)
+    window.addEventListener("scroll", handleScroll)
   })
 
-if (!scrolled){
-   return (
-    <StyledHeader>
-        <img></img>
-    </StyledHeader>
-   )
-}
-else {
+  if (!scrolled) {
     return (
-        <StyledScrolledHeader>
-
-
-        </StyledScrolledHeader>
+      <StyledHeader>
+        <HeaderElement>
+          <Link to="">
+            <img src={MenuIcon} alt="Menu" />
+          </Link>
+        </HeaderElement>
+        <HeaderElement>
+          <Link to="">Logo</Link>
+        </HeaderElement>
+        <HeaderElement>
+          <Link to="">Site in English</Link>
+        </HeaderElement>
+      </StyledHeader>
     )
+  } else {
+    return (
+      <StyledScrolledHeader>
+        <HeaderElement>
+          <Link to="">
+            <img src={MenuIcon} alt="Menu" />
+          </Link>
+        </HeaderElement>
+        <HeaderElement>
+          <Link to="">Logo</Link>
+        </HeaderElement>
+        <HeaderElement>
+          <Link to="">Site in English</Link>
+        </HeaderElement>
+      </StyledScrolledHeader>
+    )
+  }
 }
-};
-export default Header;
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,60 @@
-import React from 'react';
-import {graphql, useStaticQuery} from "gatsby";
-import GlobalStyles from '../components/GlobalStyles';
-import CalltoActionButton from '../components/CalltoActionButton';
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import GlobalStyles from "../components/GlobalStyles"
+import CalltoActionButton from "../components/StyledButton"
+import Header from "../components/Header"
 
 function IndexPage(props) {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
-        site {
-          siteMetadata {
-            title
-          }
+      site {
+        siteMetadata {
+          title
         }
       }
-      
-    
-    `)
-    return (
-      <div>
-        <GlobalStyles/>
-          
-           <CalltoActionButton size = "small">Test</CalltoActionButton>
-           <CalltoActionButton>Noch ein Test</CalltoActionButton>
-           </div>
-    );
+    }
+  `)
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+      diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet.
+      <CalltoActionButton size="small">Test</CalltoActionButton>
+      <CalltoActionButton>Noch ein Test</CalltoActionButton>
+    </div>
+  )
 }
 
-export default IndexPage;
+export default IndexPage
