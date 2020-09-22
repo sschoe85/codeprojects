416b0750a705b4e61a07bfc02d5d9d4e409a3ed4
rheimers <regineheimers@gmail.com>
2020-09-22 16:23:10 +0200

    First design of activities section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import GlobalStyles from "../components/GlobalStyles"
 import Header from "../components/Header"
 import Welcome from "../sections/Welcome"
-import SectionHeader from "../components/SectionHeader"
+import Activities from "../sections/Activities"
 
 function IndexPage(props) {
   const data = useStaticQuery(graphql`
@@ -20,44 +20,7 @@ function IndexPage(props) {
       <GlobalStyles />
       <Header />
       <Welcome />
-      <SectionHeader>Activities</SectionHeader>
-      <p>
-        {" "}
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-        est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-        est Lorem ipsum dolor sit amet.
-      </p>
+      <Activities></Activities>
     </div>
   )
 }
---
+++ src/sections/Activities.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SectionHeader from "../components/SectionHeader"
+import Faq from "../pages/faq"
+
+function Activities(props) {
+  return (
+    <section>
+      <SectionHeader>Activities</SectionHeader>
+      <Faq></Faq>
+    </section>
+  )
+}
+
+export default Activities
