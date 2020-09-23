a752563680f95710d3932efd4c98cc47997eb0e0
EmilyCook321 <emilyacook16@gmail.com>
2020-09-23 12:31:18 +0200

    First design of About Me section & added video component

---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled"
+import React from "react"
+
+const Video = ({ videoSrcURL, width, height }) => (
+  <iframe
+    width={width}
+    height={height}
+    src={videoSrcURL}
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    frameBorder="0"
+    webkitallowfullscreen="true"
+    mozallowfullscreen="true"
+    allowFullScreen
+  />
+)
+export default Video
+
