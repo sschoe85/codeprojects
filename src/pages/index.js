774d16dd078de04915b0b5dae4625df5af0eaa87
EmilyCook321 <emilyacook16@gmail.com>
2020-09-24 17:55:15 +0200

    Clean up code, add scrollto button functionality (test required)

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,8 @@ function IndexPage(props) {
       }
     }
   `)
+  
+
   return (
     <PageLayout>
       <Welcome />
--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -15,6 +15,7 @@ function AboutMe(props) {
     <SectionAboutMe>
       <SectionHeader>About Me</SectionHeader>
       <Video
+        title="About Me"
         width="300px"
         height="300px"
         videoSrcURL="https://www.youtube-nocookie.com/embed/Cz7Cy2Ed_tk"
