6c331a568e7952e7a5fe7e2cda9f77355fa815c1
rheimers <regineheimers@gmail.com>
2020-09-30 11:39:46 +0200

    Add helmet to indexPage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,21 @@
 module.exports = {
-  siteMetadata:{
-    title: 'CodeProjects',
-    author: 'Sabine Schoetensack'
+  siteMetadata: {
+    title: "CodeProjects",
+    author: "Sabine Schoetensack",
+    keywords: "HTML, CSS, JavaScript",
+    url: "www.code-projects.de",
   },
- 
+
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'src',
-        path: `${__dirname}/src/pagecontent`
-      }
-    }, 
-    'gatsby-transformer-remark',
-    `gatsby-plugin-emotion`
+        name: "src",
+        path: `${__dirname}/src/pagecontent`,
+      },
+    },
+    "gatsby-transformer-remark",
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
