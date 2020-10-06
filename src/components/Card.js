d061679286d036701bceeb4439e8ed788f34c2ed
rheimers <regineheimers@gmail.com>
2020-10-06 15:55:29 +0200

    Add EventSection

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,69 +1,32 @@
-import React, { useState } from "react"
-import { useSpring, animated } from "react-spring"
+import React from "react"
 import styled from "@emotion/styled"
 
 const Container = styled.div`
-  text-align: center;
-  position: relative;
+  position: absolute;
   height: 300px;
-  min-width: 300px;
-`
-const Front = styled(animated.div)`
-  display: flex;
-  flex-direction: column;
-  background-image: var(--gradient);
-  cursor: pointer;
-  color: white;
-  padding: 1rem;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-`
-const Back = styled(animated.div)`
-  display: flex;
-  flex-direction: column;
-  background-image: var(--gradientSecondary);
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  padding: 1rem;
-  color: black;
+  width: 300px;
+  top: 15px;
+  left: 15px;
+  background-color: #111;
   overflow-y: scroll;
-  li {
-    text-align: left;
-  }
-  & ::-webkit-scrollbar {
-    width: 10px;
-  }
-  & ::-webkit-scrollbar-track {
-    background: #111;
-  }
-  & ::-webkit-scrollbar-thumb {
-    background: white;
-  }
 `
 
-function Card({ front, back }) {
-  const [flipped, setFlipped] = useState(false)
-  const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px), rotateX(${flipped ? 180 : 0})deg`,
-    config: { mass: 5, tension: 500, friction: 80 },
-  })
+const Border = styled.div`
+  position: relative;
+  height: 330px;
+  width: 330px;
+  background-image: var(--gradient);
+  margin: 0rem 1rem;
+`
+
+function Card({ title, description }) {
   return (
-    <Container onClick={() => setFlipped(!flipped)}>
-      <Front style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
-        <div dangerouslySetInnerHTML={front}></div>
-      </Front>
-      <Back
-        style={{
-          opacity,
-          transform: transform.interpolate(t => `${t} rotateX(180deg)`),
-        }}
-      >
-        <div dangerouslySetInnerHTML={back}></div>
-      </Back>
-    </Container>
+    <Border>
+      <Container>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Container>
+    </Border>
   )
 }
 
